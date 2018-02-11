071fa28d3ce6f2a7a81a7c04f094d0855f0a1324
lolofx <lolofx@gmail.com>
2018-02-11 19:11:54 +0100

    MAJ + commenatires => code fonctionnel

--- 5.ComIR/ComIR.ino
+++ 5.ComIR/ComIR.ino
@@ -5,7 +5,7 @@
 
 long val;
 int IRpin = 10;
-int LEDpin = 4;
+//int LEDpin = 4;
 
 IRrecv irrecv(IRpin);
 decode_results results;
@@ -13,42 +13,44 @@ decode_results results;
 void setup(){
     Serial.begin(9600);
     irrecv.enableIRIn(); //active l'IR sur l'entrée
-    pinMode(LEDpin,OUTPUT);
+    //pinMode(LEDpin,OUTPUT);
 }
 
 
 void dump(decode_results *results)
 {
-    int count = results->rawlen; //recup de la longueur du signal
+    int count = results->rawlen; //recup de la longueur du signal (d'apres me monitor, 68)
     Serial.println("signal recu");
     Serial.print("Longueur du signal (");
-    Serial.print(count,DEC);
+    Serial.print(count,DEC); //affiche les valeurs sur le monitor arduino
     Serial.println(") : ");
 
-    for(int i=0;i<count;i++){ //pour chaque resultat de la liste recupérer
+    for(int i=0;i<count;i++){ //pour chaque resultat de la liste recupérer => on a la frequence du signal
         if((i%2)== 1){ //resultats impair = 0v et les resultats pair 5v
         //Resultat pairs
-        Serial.println(results->rawbuf[i]*USECPERTICK,DEC); //1 tick tous les 50µs donc traduction de la valeur du signal en µs
+        Serial.println(results->rawbuf[i]*USECPERTICK,DEC); //1 tick tous les 50µs donc traduction de la valeur du signal en µs (1/T pour avoir la frequence)
+        //Ici recup des valeurs positives donc 5V => 1 en binaire
         }else{
         //resultats impairs
         Serial.println(-(int)results->rawbuf[i]*USECPERTICK,DEC);
+        //Ici recup des valeurs negative donc 0V => 0 en binaire
         }
     }
 
 }
 
 void loop(){
-if(irrecv.decode(&results)==1){
+if(irrecv.decode(&results)==1){//reception d'un signal
     //val = results.value;
     //Serial.println(val,DEC);//sort sur le moniteur la valeur du bouton en DEC
-    dump(&results);
+    dump(&results); //Analyse du signal
     irrecv.resume();//pres pour la reception de la prochaine valeur
 }
-if(val== 16738455){ //bouton 0 de la telecommande
-    digitalWrite(LEDpin,HIGH);
-}else{
-    digitalWrite(LEDpin,LOW);
-}
+// if(val== 16738455){ //bouton 0 de la telecommande
+//     digitalWrite(LEDpin,HIGH);
+// }else{
+//     digitalWrite(LEDpin,LOW);
+// }
 
 }
 
